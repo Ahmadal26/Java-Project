deef59d13e8c953b8b6e6a175dddbdee1b72ea45
Ahmad Y Y M Kh Musallam <83317@kfh.com>
2024-01-10 08:48:12 +0300

    Done with Bank Account card

--- Java-project/src/BankAccount.java
+++ Java-project/src/BankAccount.java
@@ -1,37 +1,53 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class BankAccount {
 
     private int accountNumber;
     private double balance;
-    private double[] transactionHistory;
+    private List<String> transactionHistory;
 
-    public BankAccount(int accountNumber, double balance, double[] transactionHistory) {
+    public BankAccount(int accountNumber, double balance) {
         this.accountNumber = accountNumber;
         this.balance = balance;
-        this.transactionHistory = transactionHistory;
+        this.transactionHistory = new ArrayList<>();
+    }
+
+    public void deposit(double amount) {
+        if (amount > 0) {
+            balance += amount;
+            transactionHistory.add("Deposit: +$" + amount);
+            System.out.println("Deposit successful. New balance: $" + balance);
+        } else {
+            System.out.println("Invalid deposit amount. Please enter a positive value.");
+        }
+    }
+
+    public void withdraw(double amount) {
+        if (amount > 0 && amount <= balance) {
+            balance -= amount;
+            transactionHistory.add("Withdrawal: -$" + amount);
+            System.out.println("Withdrawal successful. New balance: $" + balance);
+        } else if (amount <= 0) {
+            System.out.println("Invalid withdrawal amount. Please enter a positive value.");
+        } else {
+            System.out.println("Insufficient funds for withdrawal.");
+        }
+    }
+
+    public void displayTransactionHistory() {
+        System.out.println("Transaction History:");
+        for (String transaction : transactionHistory) {
+            System.out.println(transaction);
+        }
     }
 
     public double getBalance() {
         return balance;
     }
 
-    public void setBalance(double balance) {
-        this.balance = balance;
-    }
-
     public int getAccountNumber() {
         return accountNumber;
-    }
-
-    public void setAccountNumber(int accountNumber) {
-        this.accountNumber = accountNumber;
-    }
-
-    public double[] getTransactionHistory() {
-        return transactionHistory;
-    }
-
-    public void setTransactionHistory(double[] transactionHistory) {
-        this.transactionHistory = transactionHistory;
     }
 
 }
