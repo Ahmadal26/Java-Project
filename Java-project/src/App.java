953b07bf0d4fb3798e9570c76866157b2eae7429
Ahmad Y Y M Kh Musallam <83317@kfh.com>
2024-01-10 15:46:50 +0300

    Done with 70% of the project

--- Java-project/src/App.java
+++ Java-project/src/App.java
@@ -1,47 +1,31 @@
-// import java.util.Scanner;
-// public class App {
-//     public static void main(String[] args) throws Exception {
-        
 
-//         BankAccount account1 = new BankAccount(123456, 1000);
-//         account1.deposit(500);
-//         account1.withdraw(200);
-//         account1.displayTransactionHistory();
-//         System.out.println("Current balance: $" + account1.getBalance());
-//         System.out.println("Account number: " + account1.getAccountNumber());
-
-//     }
-// }
 import java.util.Scanner;
+
 public class App {
 
     public static void main(String[] args) {
-     
+
         BankAccount userAccount = new BankAccount(123456, 1000);
 
-        User user = new User("exampleUser", "examplePassword", userAccount);
+        User user = new User("Ahmad", "123", userAccount);
 
-        BankSingleton bankSingleton = BankSingletong.getInstance();
-        
+        BankSingleton bankSingleton = BankSingleton.getInstance();
+
         bankSingleton.setUser(user);
 
         Scanner scanner = new Scanner(System.in);
 
         try {
-            
+
             System.out.print("Enter username: ");
             String enteredUsername = scanner.nextLine();
 
             System.out.print("Enter password: ");
             String enteredPassword = scanner.nextLine();
 
-         
-              User authenticateUser = bankSingleton.authenticateUser(enteredUsername, enteredPassword);
-
-            if (authenticateUser!= null && bankSingleton.authenticateUser(enteredUsername,enteredPassword)) {
+            if (bankSingleton.authenticateUser(enteredUsername, enteredPassword)) {
                 System.out.println("Authentication successful!");
 
-               
                 userAccount.deposit(500);
                 userAccount.withdraw(200);
                 userAccount.displayTransactionHistory();
@@ -54,7 +38,7 @@ public class App {
         } catch (Exception e) {
             System.out.println("An error occurred: " + e.getMessage());
         } finally {
-          
+
             scanner.close();
         }
     }
--- Java-project/src/User.java
+++ Java-project/src/User.java
@@ -1,13 +1,17 @@
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class User {
 
     private String userName;
     private String password;
-    private BankAccount account;
+    private List<BankAccount> accounts;
 
-    public User(String userName, String password, BankAccount account) {
+    public User(String userName, String password) {
         this.userName = userName;
         this.password = password;
-        this.account = account;
+        this.accounts = new ArrayList<>();
     }
 
     public String getUserName() {
@@ -26,16 +30,31 @@ public class User {
         this.password = password;
     }
 
-    public BankAccount getAccount() {
-        return account;
+    public List<BankAccount> getAccounts() {
+        return accounts;
     }
 
-    public void setAccount(BankAccount account) {
-        this.account = account;
-
-
-       
-    
+    public boolean isUserNameUnique(List<User> existingUsers, String newUserName) {
+        for (User user : existingUsers) {
+            if (user.getUserName().equals(newUserName)) {
+                return false;
+            }
+        }
+        return true;
     }
 
+    public void createBankAccount(List<User> existingUsers) {
+        if (isUserNameUnique(existingUsers, userName) && accounts.isEmpty()) {
+
+            int accountNumber = (int) (Math.random() * 1000000);
+
+            BankAccount newAccount = new BankAccount(accountNumber, 0.0);
+
+            accounts.add(newAccount);
+
+            System.out.println("Bank account created successfully. Account number: " + accountNumber);
+        } else {
+            System.out.println("Username is not unique or user already has a bank account.");
+        }
+    }
 }
